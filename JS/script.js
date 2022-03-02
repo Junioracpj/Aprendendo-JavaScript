ff03ccae30ed9166dd281b80a9c9866bcb7ec852
Antonio Carlos Pereira Junior <100253996+Junioracpj@users.noreply.github.com>
2022-03-02 08:52:36 -0300

    Calculadora básica via Navegador

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+let num1 = prompt('digite um numero');
+let num2 = prompt('digite outro numero');
+num1 = Number(num1);
+num2 = Number(num2);
+let resul = num1 + num2;
+alert(`${resul} é o resultado da soma`);
+alert(`${num1 * num2} é o resultado da multiplicação`);
+alert(`${num1 / num2} é o resultado da divisão`);
+alert(`${num1 % num2} é a sobra da divisão`);
+alert(`${num1 ** num2} é o resultado da potenciação`);
+
